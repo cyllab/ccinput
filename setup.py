14c50b681a0dcd4bfece6c04c0cd92ea543464a5
Raphaël Robidas <Raphael.Robidas@USherbrooke.ca>
2022-01-16 10:52:18 -0500

    Added publication to PyPI in CI

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(name='ccinput',
-        version='0.1',
+        version='1.0.0',
         description='Computational Chemistry Input Generator',
         url='http://github.com/cyllab/ccinput',
         author='Raphaël Robidas',
         author_email='Raphael.Robidas@USherbrooke.ca',
         license='BSD 3-Clause',
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Topic :: Scientific/Engineering :: Chemistry",
+        ],
         packages=find_packages(),
         entry_points={
             'console_scripts': [
@@ -22,4 +29,5 @@ setup(name='ccinput',
         tests_require=[
             'nose',
             ],
+        python_requires=">=3.6",
         zip_safe=False)
