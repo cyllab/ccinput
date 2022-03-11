e8f8e367086e025bdbc8024c660a511ca75b73c3
Lint Action <lint-action@samuelmeuli.com>
2022-03-11 02:17:58 +0000

    Code style standardization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,39 +4,39 @@ from os.path import basename
 from pathlib import Path
 import ccinput
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 
-project = 'ccinput'
-copyright = '2022, Raphaël Robidas'
-author = 'Raphaël Robidas'
+project = "ccinput"
+copyright = "2022, Raphaël Robidas"
+author = "Raphaël Robidas"
 
-release = 'v' + ccinput.__version__
+release = "v" + ccinput.__version__
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autosectionlabel',
-    'sphinxarg.ext',
-    'sphinx_toolbox.collapse',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosectionlabel",
+    "sphinxarg.ext",
+    "sphinx_toolbox.collapse",
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
 
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 exclude_patterns = []
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
 autosectionlabel_prefix_document = True
 
 
@@ -53,27 +53,42 @@ except ImportError:
 from docutils.parsers.rst import Directive
 from docutils import nodes, statemachine
 
+
 class ExecDirective(Directive):
     """Execute the specified python code and insert the output into the document"""
+
     has_content = True
 
     def run(self):
         oldStdout, sys.stdout = sys.stdout, StringIO()
 
-        tab_width = self.options.get('tab-width', self.state.document.settings.tab_width)
-        source = self.state_machine.input_lines.source(self.lineno - self.state_machine.input_offset - 1)
+        tab_width = self.options.get(
+            "tab-width", self.state.document.settings.tab_width
+        )
+        source = self.state_machine.input_lines.source(
+            self.lineno - self.state_machine.input_offset - 1
+        )
 
         try:
-            exec('\n'.join(self.content))
+            exec("\n".join(self.content))
             text = sys.stdout.getvalue()
             lines = statemachine.string2lines(text, tab_width, convert_whitespace=True)
             self.state_machine.insert_input(lines, source)
             return []
         except Exception:
-            return [nodes.error(None, nodes.paragraph(text = "Unable to execute python code at %s:%d:" % (basename(source), self.lineno)), nodes.paragraph(text = str(sys.exc_info()[1])))]
+            return [
+                nodes.error(
+                    None,
+                    nodes.paragraph(
+                        text="Unable to execute python code at %s:%d:"
+                        % (basename(source), self.lineno)
+                    ),
+                    nodes.paragraph(text=str(sys.exc_info()[1])),
+                )
+            ]
         finally:
             sys.stdout = oldStdout
 
-def setup(app):
-    app.add_directive('exec', ExecDirective)
 
+def setup(app):
+    app.add_directive("exec", ExecDirective)
